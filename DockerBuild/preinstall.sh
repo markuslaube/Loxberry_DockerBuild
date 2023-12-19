4444c92184ee978a1e8879b9c267caf37c542942
Markus Laube <markus.laube@noris.de>
2023-12-19 11:46:25 +0100

    Add files via upload
    Add Preinstallation Script for details to change image in the near of DietPi (https://github.com/MichaIng/DietPi)

---
+++ DockerBuild/preinstall.sh
@@ -0,0 +1,12 @@
+cd /tmp
+git clone https://github.com/MichaIng/DietPi.git 
+mkdir /boot/dietpi
+cp /tmp/DietPi/dietpi.txt /boot/
+cd /tmp/DietPi/dietpi
+cp -r * /boot/dietpi/
+echo 'G_DIETPI_INSTALL_STAGE=-2' > /boot/dietpi/.install_stage 
+bash /boot/dietpi/func/dietpi-obtain_hw_model
+. /boot/dietpi/func/dietpi-globals
+G_VERSIONDB_SAVE
+cd /tmp
+rm -rf /tmp/DietPi
