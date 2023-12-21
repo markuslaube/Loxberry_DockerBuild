f7fedbbe30c1c4e25df26d35e30e320b6cad2b27
Markus Laube <markus.laube@noris.de>
2023-12-21 11:05:05 +0100

    Update preinstall.sh
    add /boot/docker/.prebuild for docker entry point script

--- DockerBuild/preinstall.sh
+++ DockerBuild/preinstall.sh
@@ -1,11 +1,13 @@
 cd /tmp
-git clone https://github.com/MichaIng/DietPi.git 
+git clone https://github.com/MichaIng/DietPi.git
 mkdir /boot/dietpi
-mkdir /boot/loxberry/.docker
+mkdir /boot/loxberry/
+mkdir /boot/docker/
+touch /boot/docker/.prebuild
 cp /tmp/DietPi/dietpi.txt /boot/
 cd /tmp/DietPi/dietpi
 cp -r * /boot/dietpi/
-echo 'G_DIETPI_INSTALL_STAGE=-2' > /boot/dietpi/.install_stage 
+echo 'G_DIETPI_INSTALL_STAGE=-2' > /boot/dietpi/.install_stage
 bash /boot/dietpi/func/dietpi-obtain_hw_model
 . /boot/dietpi/func/dietpi-globals
 G_VERSIONDB_SAVE
